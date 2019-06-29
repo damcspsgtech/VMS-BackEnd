664be313e2ed342f0fba908ab13e7740f465f974
Akhilesh <akhilramkee123@gmail.com>
2019-06-29 13:26:40 +0530

    Changed the development port. Added support for nodemon

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const path = require('path');
 const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
+const debug = require('debug');
 
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -14,5 +15,5 @@ app.use('/',require('./src/routes/index'));
 app.use('/login',require('./src/routes/loginRoutes.js'));
 
 app.listen(port,()=>{
-    console.log(`Listening to port ${port}`);
+    debug(`Listening to port ${port}`);
 });
