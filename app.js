7daeabdbc5b10e8947064375cefe9ad76c1ae528
Aakash Hemadri <aakashhemadri123@gmail.com>
2019-07-02 23:10:32 +0530

    Update to login.js

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const debug = require('debug');
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/',require('./src/routes/index'));
-app.use('/students',require('./src/routes/students'));
-app.use('/faculty',require('./src/routes/faculty'));
-app.use('/login',require('./src/routes/loginRoutes.js'));
-app.use('/setting',require('./src/routes/setting'));
+app.use('/', require('./src/routes/index'));
+app.use('/api/students', require('./src/routes/students'));
+app.use('/api/faculty', require('./src/routes/faculty'));
+app.use('/api/login', require('./src/routes/login'));
+app.use('/api/setting', require('./src/routes/setting'));
 
-app.listen(port,()=>{
-    debug(`Listening to port ${port}`);
+app.listen(port, () => {
+  debug(`Listening to port ${port}`);
 });
