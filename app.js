8588c76055a7bffe206969c9c8590e63770ac503
Aakash Hemadri <aakashhemadri123@gmail.com>
2019-07-04 19:31:24 +0530

    Change to express-generator

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-const express = require('express');
-const path = require('path');
-const sqlite3 = require('sqlite3');
-const bodyParser = require('body-parser');
-const debug = require('debug');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
+var app = express();
 
-const app = express();
-const port = process.env.PORT || 4000;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use('/', require('./src/routes/index'));
-app.use('/api/students', require('./src/routes/students'));
-app.use('/api/faculty', require('./src/routes/faculty'));
-app.use('/api/login', require('./src/routes/login'));
-app.use('/api/setting', require('./src/routes/setting'));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-  debug(`Listening to port ${port}`);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
