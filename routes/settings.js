da74eedb09a2ca3f7a721348486c425b40cea3ea
Aakash Hemadri <aakashhemadri123@gmail.com>
2019-07-16 01:22:52 +0530

    Added CRUD operations on Batch, Course and Generic
    /batch
    /batch/add
    /batch/update
    /batch/delete
    /course
    /course/add
    /course/delete

--- routes/settings.js
+++ routes/settings.js
@@ -18,17 +18,6 @@ settingRouter.get('/', (req, res) => {
   */
   db.setting.findOne({ where: { id: 1 } }).then((generic) => res.send(generic))
 });
-
-/*
-* *GET* response to batch setting request.
-*/
-settingRouter.get('/batch', (req, res) => {
-  db.batch.findAll({ where: { active: true } })
-    .then((batches) => {
-      res.send(batches);
-    });
-});
-
 /*
 * *POST* response to setting generic.
 */
@@ -36,13 +25,12 @@ settingRouter.post('/', (req, res) => {
   db.batch.findAll({ where: { active: true } })
     .then((batches) => {
       let sum = 0;
-      for (let i = 0; i < batches.length; i++) {
-        sum += batches[i].count;
+      for (batch in batches) {
+        sum += batch.count;
       }
       db.setting.upsert({
         id: '1',
         count: sum,
-        session: req.body.session,
         student_form: req.body.student_form,
         report_form: req.body.report_form,
         faculty_form: req.body.faculty_form,
@@ -56,17 +44,153 @@ settingRouter.post('/', (req, res) => {
     })
 })
 
-settingRouter.post('/batch', (req, res) => {
+
+/*
+* *GET* response to batch setting request.
+*/
+settingRouter.get('/batch', (req, res) => {
+  db.batch.findAll({
+    order: [['id', 'DESC']],
+    include: ['Tutor', 'Course']
+  })
+    .then((batches) => {
+      if (batches !== null) {
+        res.send({
+          result: 'success',
+          batches
+        })
+      }
+      else {
+        res.send({
+          result: 'failed'
+        })
+      }
+
+    });
+});
+settingRouter.post('/batch/update', (req, res) => {
   db.batch.upsert({
     id: req.body.batch_id,
-    code: req.body.batch_code,
     count: req.body.batch_count,
     email: req.body.batch_email,
-    year: req.body.batch_year,
-    tutor: req.body.batch_tutor,
-    color: req.body.batch_color
+    course_id: req.body.batch_course.id,
+    tutor_id: req.body.batch_tutor.id,
+    color: req.body.batch_color,
+    active: req.body.batch_active,
   })
-    .then(() => res.send('Batch Settings Updated Successfuly'))
-    .catch((error) => res.send('Batch Setting Update Failed' + error));
+    .then(() => res.send({
+      result: 'success'
+    }))
+    .catch(() => res.send({
+      result: 'failed'
+    }));
+})
+settingRouter.post('/batch/add', (req, res) => {
+  db.course.findOne({ where: { id: req.body.course_id } })
+    .then((course) => {
+      if (course !== null) {
+        db.batch.findOne({
+          where: {
+            id: (req.body.batch_year).slice(2, 4) + req.body.course_id,
+            semester: req.body.batch_semester,
+          }
+        })
+          .then((batch) => {
+            if (batch === null) {
+              db.batch.create({
+                id: (req.body.batch_year).slice(2, 4) + req.body.course_id,
+                count: 40,
+                email: 'contact@googlegroups.com',
+                color: 'primary',
+                active: false,
+                semester: req.body.batch_semester,
+                year: req.body.batch_year,
+              })
+                .then((batch) => {
+                  batch.setCourse(course);
+                  db.faculty.findOne({ where: { id: 'admin' } })
+                    .then((tutor) => {
+                      batch.setTutor(tutor)
+                    }).then(() => res.send({
+                      result: 'success'
+                    }))
+
+                })
+            }
+            else {
+              res.send({
+                result: 'batch_exists'
+              });
+            }
+          })
+
+      }
+      else {
+        res.send({
+          result: 'course_dne'
+        });
+      }
+    })
+    .catch(() => res.send({
+      result: 'failed'
+    }));
+})
+settingRouter.post('/batch/delete', (req, res) => {
+  db.batch.destroy({ where: { id: req.body.batch_id } })
+    .then(() => {
+      res.send({
+        result: 'success',
+      });
+    })
+})
+/*
+* Course Routes
+*/
+settingRouter.get('/course', (req, res) => {
+  db.course.findAll({ order: [['id', 'ASC']] })
+    .then((courses) => {
+      if (courses === null) {
+        res.send({
+          result: 'course_dne',
+        })
+      }
+      else {
+        res.send({
+          result: 'success',
+          courses,
+        })
+      }
+    })
+    .catch((error) => res.send({
+      result: 'failed',
+      error
+    }))
+})
+settingRouter.post('/course/add', (req, res) => {
+  db.course.upsert({
+    id: req.body.course_id,
+    name: req.body.course_name,
+  })
+    .then(() => res.send({
+      result: 'success'
+    }))
+    .catch(() => res.send({
+      result: 'failed'
+    }))
+})
+settingRouter.post('/course/delete', (req, res) => {
+  db.course.destroy({ where: { id: req.body.course_id } })
+    .then((rows) => {
+      if (rows > 0) {
+        res.send({
+          result: 'success'
+        })
+      }
+      else {
+        res.send({
+          result: 'failed'
+        })
+      }
+    })
 })
 module.exports = settingRouter;
